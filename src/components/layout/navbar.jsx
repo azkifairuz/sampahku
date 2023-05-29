9d61e44216634cfaf5dc21cb0c5720a9e03078f8
azkifairuz <azkiajmal@gmai.com>
2023-05-30 02:13:18 +0700

    create navbar item more dinamyc

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,14 +1,40 @@
 import { RiHomeLine } from "react-icons/ri";
 import { BsTruck } from "react-icons/bs";
-import {CgProfile} from "react-icons/cg";
-import {CiLocationOn}  from "react-icons/ci";
+import { CgProfile } from "react-icons/cg";
+import { CiLocationOn } from "react-icons/ci";
+import { Link } from "react-router-dom";
 export default function Navbar() {
-     return(
+    const itemNavbar = [
+        {
+            href: "/home",
+            icon: <RiHomeLine className="cursor-pointer" size={50} />
+        },
+        {
+            href: "/location",
+            icon: <CiLocationOn className="cursor-pointer" size={50} />
+
+        },
+        {
+            href: "/history",
+            icon: <BsTruck className="cursor-pointer" size={50} />
+        },
+        {
+            href: "/profile",
+            icon: <CgProfile className="cursor-pointer" size={50} />
+
+        },
+    ]
+    return (
         <div className="py-6 px-[26px] max-w-sm w-full text-white justify-around items-center fixed bottom-0 bg-blueMain flex gap-[58px]">
-            <RiHomeLine className="cursor-pointer" size={50}/>
-            <BsTruck className="cursor-pointer" size={50}/>
-            <CiLocationOn className="cursor-pointer" size={50}/>
-            <CgProfile className="cursor-pointer" size={50}/>
+            {
+                itemNavbar.map((item) => {
+                    return (
+                        <Link to={item.href}>
+                            {item.icon}
+                        </Link>
+                    )
+                })
+            }
         </div>
-     );
+    );
 }
