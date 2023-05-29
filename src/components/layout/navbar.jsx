2fb1d476735f53aaaaf18aa9e147bc3d856776f1
azkifairuz <azkiajmal@gmai.com>
2023-05-30 00:59:51 +0700

    navbar body

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,3 +1,7 @@
-export default function Navbar(params) {
-     
+export default function Navbar() {
+     return(
+        <div className="py-6 px-[26px] bg-blueMain flex gap-[58px]">
+            
+        </div>
+     )
 }
