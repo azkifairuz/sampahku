2fe53ed626f175ab260298eadec95383d5cd8e21
azkifairuz <azkiajmal@gmai.com>
2023-05-30 02:30:33 +0700

    button navigate from onboarding to home

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from 'react-router-dom'
 import Logo from '../asset/Logo.png'
 export default function OnBoarding() {
+    const navigate = useNavigate()
     return (
         <div className="flex flex-col w-full gap-[138px] md:mx-auto md:max-w-sm bg-surface justify-center h-screen items-center">
             <img src={Logo} alt="logo" className='w-[266.36px]' />
-            <button className='rounded-lg bg-blueMain w-[266.36px] text-white py-2 px-9'>Let’s Started</button>
+            <button
+                onClick={() => navigate("/")}
+                className='rounded-lg bg-blueMain w-[266.36px] text-white py-2 px-9'>Let’s Started
+            </button>
         </div>
     )
 }
