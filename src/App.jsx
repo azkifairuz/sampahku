293fb3937508b2916b177d02104a4684732080ab
azkifairuz <azkiajmal@gmai.com>
2023-05-30 00:49:10 +0700

    create layout

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+function App() {
+    <Routes>
+        <Route path="/" element>
+            
+        </Route>
+    </Routes>
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        greenMain:'#25B56D',
+        blueMain:'#025464',
+        surface:'#'
+      }
+    },
   },
   plugins: [],
 }
