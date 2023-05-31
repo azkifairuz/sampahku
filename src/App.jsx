327d4fa53b9989f34dba863c5f8c995f6691977f
azkifairuz <azkiajmal@gmail.com>
2023-05-31 12:33:38 +0700

    feat:create route singup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NotFound from "./pages/_404";
 import Home from "./pages/beranda";
 import OnBoarding from "./pages/onboarding";
 import Login from "./pages/login";
+import Singup from "./pages/signup";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="onboarding" element={<OnBoarding />} />
                 <Route path="login" element={<Login />} />
+                <Route path="signup" element={<Singup />} />
                 <Route path="/" element={<Layout />} >
                     <Route index element={<Home />} />
                 </Route>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,7 @@
+import { useNavigate } from "react-router-dom";
 import Logo from "../asset/Logo.png";
 export default function Login() {
+  const navigate = useNavigate();
   return (
     <div className="bg-[#D3F0E2] pt-[17px] flex h-screen flex-col gap-[14px] justify-start px-[46px]">
       <div className="max-w-[130px]">
@@ -12,36 +14,59 @@ export default function Login() {
         <div className="text-[12px]">Login/daftarkan akun anda</div>
       </div>
       <div className="w-full mx-auto flex flex-col gap-[20px] justify-center px-10 bg-blueMain p-5 rounded-lg">
-        <h1 className="text-white text-center text-[15px] border-b border-b-white">Selamat Datang Di sampahku</h1>
+        <h1 className="text-white text-center text-[15px] border-b border-b-white">
+          Selamat Datang Di sampahku
+        </h1>
         <form action="" className="flex justify-center flex-col gap-[10px]">
-        <div className="flex flex-col gap-[11px]">
-        <label htmlFor="email" className="text-white text-[13px]">Email</label>
-          <input
-            className="rounded-[12px] outline-none text-[13px] bg-white h-[35px] p-2"
-            type="text"
-            id="email"
-            name="email"
-            placeholder="Masukan Email Atau Username"
-          />
-        </div>
           <div className="flex flex-col gap-[11px]">
-          <label htmlFor="pw" className="text-white  text-[13px] ">Password</label>
-          <input
-            className="rounded-[12px] outline-none text-[13px] bg-white h-[35px] p-2"
-            type="password"
-            id="pw"
-            name="pw"
-            placeholder="Masukan Password"
-          />
-          <p className="self-end text-white text-[13px]">Lupa Password?</p>
+            <label htmlFor="email" className="text-white text-[13px]">
+              Email
+            </label>
+            <input
+              className="rounded-[12px] outline-none text-[13px] bg-white h-[35px] p-2"
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Masukan Email Atau Username"
+            />
+          </div>
+          <div className="flex flex-col gap-[11px]">
+            <label htmlFor="pw" className="text-white  text-[13px] ">
+              Password
+            </label>
+            <input
+              className="rounded-[12px] outline-none text-[13px] bg-white h-[35px] p-2"
+              type="password"
+              id="pw"
+              name="pw"
+              placeholder="Masukan Password"
+            />
+            <p className="self-end text-white text-[13px]">Lupa Password?</p>
           </div>
         </form>
         <div className="flex flex-col  gap-[20px] text-center">
-            <p className="text-white text-[13px]">Atau</p>
-            <h1 className="text-white text-[12px]">Belum Memiliki Akun? <span className="text-[#49C185] cursor-pointer">Daftar</span></h1>
+          <p className="text-white text-[13px]">Atau</p>
+          <h1 className="text-white text-[12px]">
+            Belum Memiliki Akun?{" "}
+            <span
+              onClick={() => {
+                navigate("singup");
+              }}
+              className="text-[#49C185] cursor-pointer"
+            >
+              Daftar
+            </span>
+          </h1>
         </div>
       </div>
-      <button className="rounded-full w-fit p-3 bg-blueMain self-end text-white ">Selanjutnya</button>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+        className="rounded-full w-fit p-3 bg-blueMain self-end text-white "
+      >
+        Selanjutnya
+      </button>
     </div>
   );
 }
